b55fa8691f71d698e48562bdb49df1250da598f4
brentheckerman433 <brent.heckerman37@gmail.com>
2025-05-02 15:00:34 -0700

    Create CryoVault.sol

---
+++ contracts/CryoVault.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CryoVault is ERC4626 {
+    constructor(
+        IERC20 _asset,
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) ERC4626(_asset) {}
+}

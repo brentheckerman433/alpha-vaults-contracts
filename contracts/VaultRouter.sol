55b4a7a76a192a872aaac98db8ec5ecdc3638036
brentheckerman433 <brent.heckerman37@gmail.com>
2025-05-03 06:01:25 -0700

    Update VaultRouter.sol

--- contracts/VaultRouter.sol
+++ contracts/VaultRouter.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import "./CryoVault.sol";
 
 import "./HeatVault.sol";
